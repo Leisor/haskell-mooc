97a2e3eabc588adb6db82a253d009d857e0d0c14
Leskelä Heikki Johannes <hleskel@gmail.com>
2025-06-30 05:47:29 +0300

    Set4a complete

--- exercises/Set4a.hs
+++ exercises/Set4a.hs
@@ -22,6 +22,7 @@ import Data.Ord
 import qualified Data.Map as Map
 import Data.Array
 
+
 ------------------------------------------------------------------------------
 -- Ex 1: implement the function allEqual which returns True if all
 -- values in the list are equal.
@@ -35,7 +36,8 @@ import Data.Array
 -- you remove the Eq a => constraint from the type!
 
 allEqual :: Eq a => [a] -> Bool
-allEqual xs = todo
+allEqual xs = if length (nub xs) < 2 then True else False
+
 
 ------------------------------------------------------------------------------
 -- Ex 2: implement the function distinct which returns True if all
@@ -50,7 +52,8 @@ allEqual xs = todo
 --   distinct [1,2] ==> True
 
 distinct :: Eq a => [a] -> Bool
-distinct = todo
+--distinct [] = True
+distinct xs = length xs == length (nub xs)
 
 ------------------------------------------------------------------------------
 -- Ex 3: implement the function middle that returns the middle value
@@ -63,7 +66,8 @@ distinct = todo
 --   middle 'b' 'a' 'c'  ==> 'b'
 --   middle 1 7 3        ==> 3
 
-middle = todo
+middle :: Ord a => a -> a -> a -> a 
+middle a b c = head (tail (sort [a, b, c]))
 
 ------------------------------------------------------------------------------
 -- Ex 4: return the range of an input list, that is, the difference
@@ -78,8 +82,8 @@ middle = todo
 --   rangeOf [4,2,1,3]          ==> 3
 --   rangeOf [1.5,1.0,1.1,1.2]  ==> 0.5
 
-rangeOf :: [a] -> a
-rangeOf = todo
+rangeOf :: (Ord a, Num a) => [a] -> a
+rangeOf xs = last (sort xs) - head (sort xs)
 
 ------------------------------------------------------------------------------
 -- Ex 5: given a (non-empty) list of (non-empty) lists, return the longest
@@ -97,7 +101,17 @@ rangeOf = todo
 --   longest [[1,2,3],[4,5],[6]] ==> [1,2,3]
 --   longest ["bcd","def","ab"] ==> "bcd"
 
-longest = todo
+longest :: Ord a => [[a]] -> [a]
+longest [] = []
+longest [x] = x
+longest (x:xs) = longest' x xs
+    where
+        longest' currentMax [] = currentMax
+        longest' currentMax (y:ys)
+            | length y > length currentMax                    = longest' y ys
+            | length y == length currentMax && y < currentMax = longest' y ys
+            | otherwise                                       = longest' currentMax ys
+ 
 
 ------------------------------------------------------------------------------
 -- Ex 6: Implement the function incrementKey, that takes a list of
@@ -113,8 +127,12 @@ longest = todo
 --   incrementKey True [(True,1),(False,3),(True,4)] ==> [(True,2),(False,3),(True,5)]
 --   incrementKey 'a' [('a',3.4)] ==> [('a',4.4)]
 
-incrementKey :: k -> [(k,v)] -> [(k,v)]
-incrementKey = todo
+incrementKey :: (Eq k, Num v) => k -> [(k,v)] -> [(k,v)]
+incrementKey _ [] = []
+incrementKey key ((k, v):xs)
+    | key == k  = (k, v+1) : incrementKey key xs
+    | otherwise = (k, v) : incrementKey key xs
+
 
 ------------------------------------------------------------------------------
 -- Ex 7: compute the average of a list of values of the Fractional
@@ -129,7 +147,10 @@ incrementKey = todo
 -- length to a Fractional
 
 average :: Fractional a => [a] -> a
-average xs = todo
+average xs = total / count
+    where
+        total = sum xs
+        count = fromIntegral (length xs)
 
 ------------------------------------------------------------------------------
 -- Ex 8: given a map from player name to score and two players, return
@@ -148,7 +169,12 @@ average xs = todo
 --     ==> "Lisa"
 
 winner :: Map.Map String Int -> String -> String -> String
-winner scores player1 player2 = todo
+winner scores player1 player2
+    | score1 >= score2 = player1
+    | otherwise        = player2
+    where
+        score1 = Map.findWithDefault 0 player1 scores
+        score2 = Map.findWithDefault 0 player2 scores
 
 ------------------------------------------------------------------------------
 -- Ex 9: compute how many times each value in the list occurs. Return
@@ -163,7 +189,14 @@ winner scores player1 player2 = todo
 --     ==> Map.fromList [(False,3),(True,1)]
 
 freqs :: (Eq a, Ord a) => [a] -> Map.Map a Int
-freqs xs = todo
+freqs xs = freqs' xs Map.empty
+    where
+        freqs' [] freqsMap = freqsMap
+        freqs' (x:xs) freqsMap =
+            case Map.lookup x freqsMap of
+                Nothing -> freqs' xs (Map.insert x 1 freqsMap)
+                Just count -> freqs' xs (Map.insert x (count + 1) freqsMap)
+
 
 ------------------------------------------------------------------------------
 -- Ex 10: recall the withdraw example from the course material. Write a
@@ -191,7 +224,13 @@ freqs xs = todo
 --     ==> fromList [("Bob",100),("Mike",50)]
 
 transfer :: String -> String -> Int -> Map.Map String Int -> Map.Map String Int
-transfer from to amount bank = todo
+transfer from to amount bank =
+    case (Map.lookup from bank, Map.lookup to bank) of
+        (Just sum1, Just sum2)
+            | amount >= 0 && sum1 >= amount ->
+                Map.insert from (sum1 - amount) (Map.insert to (sum2 + amount) bank) 
+        _                                   -> bank
+
 
 ------------------------------------------------------------------------------
 -- Ex 11: given an Array and two indices, swap the elements in the indices.
@@ -201,7 +240,7 @@ transfer from to amount bank = todo
 --         ==> array (1,4) [(1,"one"),(2,"three"),(3,"two"),(4,"four")]
 
 swap :: Ix i => i -> i -> Array i a -> Array i a
-swap i j arr = todo
+swap i j arr = arr // [(i, arr ! j), (j, arr ! i)]
 
 ------------------------------------------------------------------------------
 -- Ex 12: given an Array, find the index of the largest element. You
@@ -212,4 +251,9 @@ swap i j arr = todo
 -- Hint: check out Data.Array.indices or Data.Array.assocs
 
 maxIndex :: (Ix i, Ord a) => Array i a -> i
-maxIndex = todo
+maxIndex arr = fst (foldr compMax (head pairs) pairs)
+    where
+        pairs = assocs arr
+        compMax (i1, v1) (i2, v2)
+            | v1 > v2 = (i1, v1)
+            | otherwise = (i2, v2)
